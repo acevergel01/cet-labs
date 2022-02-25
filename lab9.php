383ed61366497b0f741bd227eb1d4c92268c47aa
Ace Drapiza <acevergeldrapiza@gmail.com>
2022-02-25 16:49:35 +0800

    Update lab 9

--- lab9.php
+++ lab9.php
@@ -1,100 +1,167 @@
-<?PHP
+<?php
+define('NAME', 'ACE VERGEL DRAPIZA');
+define('STUDENTNUMBER', '2019-037290-MN-0');
+define('ADDRESS', 'Brgy Pembo, Makati City');
+define('EMAILADDRESS', 'acevergeldrapiza@gmail.com');
+define('CONTACTNUMBER', '09054336293');
+define('WEBADDRESS', 'avmdrapiza@iskolarngbayan.pup.edu.ph');
 define('LABTITLE', 'Laboratory Activity No. 9');
 define('DESCRIPTION', 'Handling User Input - User Registration');
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-function generate_textbox($name, $value)
-{
-    return '<input type="text" name="' . $name . '" value="' . $value . '">';
-}
-function generate_password($name, $value)
-{
-    return '<input type="password" name="' . $name . '" value="' . $value .
-        '">';
-}
-function generate_textarea($name, $value)
-{
-    return '<textarea name="' . $name . '">' . $value . '</textarea>';
-}
-function generate_checkbox($name)
-{
-    return '<input type="checkbox" name="' . $name . '">';
-}
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo LABTITLE; ?></title>
+    <style>
+        html {
+            height: 95vh;
+        }
 
-// *********************************************************
-// Items 1 to 9, get form data and assign to local variable
-// Item 1 is already given
-// *********************************************************
-// Item 1: Get lastname form data
-$lastname = isset($_POST['txtLastname']) ? $_POST['txtLastname'] : '';
-$firstname = isset($_POST['txtFirstname']) ? $_POST['txtFirstname'] : '';
-$middlename = isset($_POST['txtMiddlename']) ? $_POST['txtMiddlename'] : '';
-$emailaddress = isset($_POST['txtEmailAddress']) ? $_POST['txtEmailAddress'] : '';
-$username = isset($_POST['txtUsername']) ? $_POST['txtUsername'] : '';
-$password = isset($_POST['txtPassword']) ? $_POST['txtPassword'] : '';
-$confirmpassword = isset($_POST['txtConfirmPassword']) ? $_POST['txtConfirmPassword'] : '';
-$comments = isset($_POST['txtComments']) ? $_POST['txtComments'] : '';
-$agree = isset($_POST['txtAgree']) ? $_POST['txtAgree'] : '';
-if (count($_POST) == 0) {
-    echo '<form method="post" action="lab9.php">';
-    echo '<table>';
-    echo '<tr><td colspan=2><strong>Lab 9 - Form Processing (User 
+        header {
+            margin: 20px;
+        }
+
+        body {
+            height: 100%;
+        }
+
+        header h3,
+        h1 {
+            margin: 0;
+        }
+
+        .nav {
+            border-radius: 5px;
+            border: solid 2px grey;
+            display: inline-block;
+            width: 20%;
+            height: 100%;
+            margin-right: 20px;
+        }
+    </style>
+</head>
+
+<body style="
+  display:flex; 
+  flex-direction:column; ">
+    <header style="margin: 20px;">
+
+        <?PHP include 'header.php'; ?>
+        <div style=" display: flex;
+    flex: 1;margin-bottom:10px">
+
+            <?PHP include 'nav.php'; ?>
+            <div style="display: inline-block;
+    border-radius: 5px;
+    border: solid 2px grey;padding:20px;text-align:center">
+                <?php
+
+
+                function generate_textbox($name, $value)
+                {
+                    return '<input type="text" name="' . $name . '" value="' . $value . '">';
+                }
+                function generate_password($name, $value)
+                {
+                    return '<input type="password" name="' . $name . '" value="' . $value .
+                        '">';
+                }
+                function generate_textarea($name, $value)
+                {
+                    return '<textarea name="' . $name . '">' . $value . '</textarea>';
+                }
+                function generate_checkbox($name)
+                {
+                    return '<input type="checkbox" name="' . $name . '">';
+                }
+
+                // *********************************************************
+                // Items 1 to 9, get form data and assign to local variable
+                // Item 1 is already given
+                // *********************************************************
+                // Item 1: Get lastname form data
+                $lastname = isset($_POST['txtLastname']) ? $_POST['txtLastname'] : '';
+                $firstname = isset($_POST['txtFirstname']) ? $_POST['txtFirstname'] : '';
+                $middlename = isset($_POST['txtMiddlename']) ? $_POST['txtMiddlename'] : '';
+                $emailaddress = isset($_POST['txtEmailAddress']) ? $_POST['txtEmailAddress'] : '';
+                $username = isset($_POST['txtUsername']) ? $_POST['txtUsername'] : '';
+                $password = isset($_POST['txtPassword']) ? $_POST['txtPassword'] : '';
+                $confirmpassword = isset($_POST['txtConfirmPassword']) ? $_POST['txtConfirmPassword'] : '';
+                $comments = isset($_POST['txtComments']) ? $_POST['txtComments'] : '';
+                $agree = isset($_POST['txtAgree']) ? $_POST['txtAgree'] : '';
+                if (count($_POST) == 0) {
+                    echo '<form method="post" action="lab9.php">';
+                    echo '<table>';
+                    echo '<tr><td colspan=2><strong>Lab 9 - Form Processing (User 
 Registration)</td></tr>';
-    // **************************************************************
-    // Item 10 to 18, generate html form elements using the functions
-    // declared in No. 4. Item 10 is already given
-    // **************************************************************
-    // Item 10: Generate lastname textbox
-    echo '<tr><td>Lastname</td><td>',
-    generate_textbox('txtLastname', $lastname),
-    '</td></tr>';
-    echo '<tr><td>Firstname</td><td>',
-    generate_textbox('txtFirstname', $firstname),
-    '</td></tr>';
-    echo '<tr><td>Middlename</td><td>',
-    generate_textbox('txtMiddlename', $middlename),
-    '</td></tr>';
-    echo '<tr><td>Email Address</td><td>',
-    generate_textbox('txtEmailAddress', $emailaddress),
-    '</td></tr>';
-    echo '<tr><td>Username</td><td>',
-    generate_textbox('txtUsername', $username),
-    '</td></tr>';
-    echo '<tr><td>Password</td><td>',
-    generate_password('txtPassword', $password),
-    '</td></tr>';
-    echo '<tr><td>Confirm Password</td><td>',
-    generate_password('txtConfirmPassword', $confirmpassword),
-    '</td></tr>';
-    echo '<tr><td>Comment</td><td>',
-    generate_textarea('txtComments', $comments),
-    '</td></tr>';
-    echo '<tr><td>Agree</td><td>',
-    generate_checkbox('txtAgree', $agree),
-    '</td></tr>';
-    echo '<tr><td colspan=2>';
-    echo '<input type="submit" name="btnRegister" value="Register">&nbsp;';
-    echo '<input type="reset" name="btnReset" value="Reset">&nbsp;';
-    echo '</td></tr>';
-    echo '</table>';
-    echo '</form>';
-} else {
-    echo '<table>';
-    echo '<tr><td colspan=2><strong>Lab 9 - Form Processing (User 
+                    // **************************************************************
+                    // Item 10 to 18, generate html form elements using the functions
+                    // declared in No. 4. Item 10 is already given
+                    // **************************************************************
+                    // Item 10: Generate lastname textbox
+                    echo '<tr><td>Lastname</td><td>',
+                    generate_textbox('txtLastname', $lastname),
+                    '</td></tr>';
+                    echo '<tr><td>Firstname</td><td>',
+                    generate_textbox('txtFirstname', $firstname),
+                    '</td></tr>';
+                    echo '<tr><td>Middlename</td><td>',
+                    generate_textbox('txtMiddlename', $middlename),
+                    '</td></tr>';
+                    echo '<tr><td>Email Address</td><td>',
+                    generate_textbox('txtEmailAddress', $emailaddress),
+                    '</td></tr>';
+                    echo '<tr><td>Username</td><td>',
+                    generate_textbox('txtUsername', $username),
+                    '</td></tr>';
+                    echo '<tr><td>Password</td><td>',
+                    generate_password('txtPassword', $password),
+                    '</td></tr>';
+                    echo '<tr><td>Confirm Password</td><td>',
+                    generate_password('txtConfirmPassword', $confirmpassword),
+                    '</td></tr>';
+                    echo '<tr><td>Comment</td><td>',
+                    generate_textarea('txtComments', $comments),
+                    '</td></tr>';
+                    echo '<tr><td>Agree</td><td>',
+                    generate_checkbox('txtAgree', $agree),
+                    '</td></tr>';
+                    echo '<tr><td colspan=2>';
+                    echo '<input type="submit" name="btnRegister" value="Register">&nbsp;';
+                    echo '<input type="reset" name="btnReset" value="Reset">&nbsp;';
+                    echo '</td></tr>';
+                    echo '</table>';
+                    echo '</form>';
+                } else {
+                    echo '<table>';
+                    echo '<tr><td colspan=2><strong>Lab 9 - Form Processing (User 
 Registration)</td></tr>';
-    echo '<tr><td colspan=2 align=center><strong>FORM SUBMITTED</td></tr>';
-    // ****************************************************************
-    // For items 20 to 28, display submitted form data inside <td></td>
-    // Item 20 already given
-    // ****************************************************************
-    // Item 20: display lastname data
-    echo '<tr><td>Lastname</td><td>', $lastname, '</td></tr>';
-    echo '<tr><td>Firstname</td><td>', $firstname, '</td></tr>';
-    echo '<tr><td>Middle Name</td><td>', $middlename, '</td></tr>';
-    echo '<tr><td>Email Address</td><td>', $emailaddress, '</td></tr>';
-    echo '<tr><td>Username</td><td>', $username, '</td></tr>';
-    echo '<tr><td>Password</td><td>', $password, '</td></tr>';
-    echo '<tr><td>Confirm Password</td><td>', $confirmpassword, '</td></tr>';
-    echo '<tr><td>Comments</td><td>', $comments, '</td></tr>';
-    echo '<tr><td>Agree</td><td>', $agree, '</td></tr>';
-    echo '</table>';
-}
+                    echo '<tr><td colspan=2 align=center><strong>FORM SUBMITTED</td></tr>';
+                    // ****************************************************************
+                    // For items 20 to 28, display submitted form data inside <td></td>
+                    // Item 20 already given
+                    // ****************************************************************
+                    // Item 20: display lastname data
+                    echo '<tr><td>Lastname</td><td>', $lastname, '</td></tr>';
+                    echo '<tr><td>Firstname</td><td>', $firstname, '</td></tr>';
+                    echo '<tr><td>Middle Name</td><td>', $middlename, '</td></tr>';
+                    echo '<tr><td>Email Address</td><td>', $emailaddress, '</td></tr>';
+                    echo '<tr><td>Username</td><td>', $username, '</td></tr>';
+                    echo '<tr><td>Password</td><td>', $password, '</td></tr>';
+                    echo '<tr><td>Confirm Password</td><td>', $confirmpassword, '</td></tr>';
+                    echo '<tr><td>Comments</td><td>', $comments, '</td></tr>';
+                    echo '<tr><td>Agree</td><td>', $agree, '</td></tr>';
+                    echo '</table>';
+                }
+                ?>
+            </div>
+
+        </div>
+        <?PHP include 'footer.php'; ?>
+</body>
+
+</html>
