2abc4d4a23d4736a011b5f4360d3aa27eff49bd6
Ace Drapiza <acevergeldrapiza@gmail.com>
2022-02-25 16:49:19 +0800

    Update lab 8

--- lab8.php
+++ lab8.php
@@ -1,3 +1,13 @@
+<?php
+define('NAME', 'ACE VERGEL DRAPIZA');
+define('STUDENTNUMBER', '2019-037290-MN-0');
+define('ADDRESS', 'Brgy Pembo, Makati City');
+define('EMAILADDRESS', 'acevergeldrapiza@gmail.com');
+define('CONTACTNUMBER', '09054336293');
+define('WEBADDRESS', 'avmdrapiza@iskolarngbayan.pup.edu.ph');
+define('LABTITLE', 'Laboratory Activity No. 1');
+define('DESCRIPTION', 'Hello World');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,270 +15,137 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php echo LABTITLE; ?></title>
+    <style>
+        html {
+            height: 95vh;
+        }
 
-    <link rel="stylesheet" href="css/design.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="" crossorigin>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Qahiri&family=Rampart+One&family=Rubik+Beastly&family=Sigmar+One&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Qahiri&family=Rampart+One&family=Rubik+Beastly&family=Shadows+Into+Light&family=Sigmar+One&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Vujahday+Script&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Qahiri&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Qahiri&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Qahiri&display=swap" rel="stylesheet">
+        header {
+            margin: 20px;
+        }
 
+        body {
+            height: 100%;
+        }
 
+        header h3,
+        h1 {
+            margin: 0;
+        }
 
-
-
-    <img src="break.png" style="width: 800px; height: 400px; position:absolute; right:100px; top:5px">
-
-
-    <div style="text-align:justify ; position:absolute ;left: 50px; right:50px ; ">
-
-
-
-
-        <h1 style=" color: black; font-family: 'Vujahday Script', cursive;  font-size: 40px; text-align:center; top:10px; text-indent:-300px"> Lindsay Mae D. Daza </h1>
-
-
-
-
-        <h2 style="color:black; font-family: 'PT Sans Narrow', sans-serif; font-size: 15px; text-align: center; text-indent: -397px">
-            <?php echo "Student  ID: 2019-04471-MN-0 "; ?>
-            <h2 style="color:black; font-family: 'PT Sans Narrow', sans-serif; font-size: 15px; text-align: center; text-indent:-320px">
-                <?php echo " Address: Macopa St., Comembo Makati ";
-                echo "<br />"; ?>
-
-                <h2 style="color:black; font-family: 'PT Sans Narrow', sans-serif; font-size: 15px; text-align: center; text-indent: -380px">
-                    <?php echo "Contact Number: 09669035646 ";
-                    echo "<br />"; ?>
-
-
-                    <h2 style="color:black; font-family: 'PT Sans Narrow', sans-serif; font-size: 15px; text-align: center; text-indent: -360px">
-                        <?php echo "E-mail: lindsaydaza16@gmail.com";
-                        echo "<br />"; ?>
-                        <h2 style="color:black; font-family: 'PT Sans Narrow', sans-serif; font-size: 15px; text-align: center; text-indent: -260px">
-                            <?php echo "Webmail: lmddaza@iskolarngbayan.pup.edu.ph";
-                            echo "<br />"; ?>
-                        </h2>
-                        <h2 style="color:black; font-family: 'PT Sans Narrow', sans-serif; font-size: 15px; text-align: center; text-indent: -225px">
-                            <?php echo "LABORATORY #8| Array Manipulations (Word Counter)";
-                            echo "<br />"; ?>
-                        </h2>
-
-    </div>
-
+        .nav {
+            border-radius: 5px;
+            border: solid 2px grey;
+            display: inline-block;
+            width: 20%;
+            height: 100%;
+            margin-right: 20px;
+        }
+    </style>
 </head>
 
-<img src="lindsay.jpg" class="lindsay" alt="lindsay" style=" height:300px; position:absolute; width:300px; border-radius: 50%; left:100px; top:50px">
+<body style="
+  display:flex; 
+  flex-direction:column; ">
+    <header style="margin: 20px;">
+
+        <?PHP include 'header.php'; ?>
+        <div style=" display: flex;
+    flex: 1;margin-bottom:10px">
+
+            <?PHP include 'nav.php'; ?>
+            <div style="display: inline-block;
+    border-radius: 5px;
+    border: solid 2px grey;padding:20px;text-align:center">
+                <?php
+
+                $text = (isset($_POST['text'])) ? $_POST['text'] : '';
+                echo '<form method="post">';
+                echo 'Enter text:<br />';
+                echo '<textarea name="text" style="width:500px">';
+                echo $text;
+                echo '</textarea><br />';
+                echo '<input type=submit value="Process">&nbsp;';
+                echo '<input type=reset>&nbsp;';
+                echo '</form>';
+                $text = preg_replace('/[^a-zA-Z0-9 ]/', '', trim($text));
+
+                $colors[] = '#00CCFF';
+                // No 1. Add at least 10 color values to array $colors
+                $colors[0] = '#000000';
+                $colors[1] = '#FF0000';
+                $colors[2] = '#FFA500';
+                $colors[3] = '#FFFF00';
+                $colors[4] = '#00FF00';
+                $colors[5] = '#0000FF';
+                $colors[6] = '#4B0082';
+                $colors[7] = '#8F00FF';
+                $colors[8] = '#964B00';
+                $colors[9] = '#FFFFFF';
+
+                if ($text <> '') {
+                    // No. 2. Convert the $text to lowercase and assign to $text
+                    $text = strtolower($text);
+
+                    // No. 3. Explode $text into array andassign to $allwords
+                    $allwords = explode(' ', $text);
+
+                    // No. 4. Remove duplicate items and assign to $uniquewordlist
+                    $uniquewordlist = array_unique($allwords);
+
+                    // No. 5. sort the array $uniquewordlist
+                    sort($uniquewordlist);
+
+                    // No. 6. Check thefirst element of $uniquewordlist if equal to space
+                    if ($uniquewordlist == '') {
+                        // No. 7. Remove the first element from $uniquewordlist
+                        $deleteditem = array_shift($uniquewordlist);
+                    }
+
+                    foreach ($uniquewordlist as $word)
+                        $counter[$word] = 0;
+
+                    $totalwords = 0;
+                    foreach ($allwords as $word) {
+                        if ($word != '') {
+                            $counter[$word]++;
+                            $totalwords++;
+                        }
+                    }
+
+                    echo '<table border=1 style="width:500px">';
+                    echo '<tr><td colspan=4 align=center>Array Manipulation -Word Counter</td></tr>';
+                    echo '<tr><td>No.</td><td>Word</td><td>Frequency</td><td>Percent</td></tr>';
+                    $i = 1;
+                    foreach ($uniquewordlist as $word) {
+                        // No. 8. Get the current element of array $colors (IAP function)
+                        $color = current($colors);
+
+                        $length = number_format($counter[$word] / $totalwords * 100.00, 2);
+                        echo '<tr>';
+                        echo '<td>', $i, '.</td>';
+                        echo '<td>', $word, '</td>';
+                        echo '<td>', $counter[$word], '</td>';
+                        echo '<td style="background:', $color, '">', $length, '%</td>';
+                        echo '</tr>';
+
+                        // No. 9. Move the internal array pointer of $colors to next element
+                        next($colors);
+
+                        if (current($colors) == false) {
+                            // No. 10. Reset internal array pointer of $colors
+                            reset($colors);
+                        }
+                        $i++;
+                    }
+                    echo '</table>';
+                }
+                ?>
+            </div>
+
+        </div>
+        <?PHP include 'footer.php'; ?>
 </body>
 
-
-
-
-<div style="border: 4px solid black;  margin: 40px;  position:absolute; top: 400px; bottom:400px;width:500px; height: 2500px; left:30px; background-image: url(https://i.pinimg.com/564x/c3/af/bb/c3afbb1fe51fd1e49bce5ae3d1fcebab.jpg); background-size:cover;object-fit:contain">
-
-
-
-    <h1 style=" color: brown; font-family: 'PT Sans Narrow', sans-serif;  font-size: 40px; text-align:center; top:10px; text-indent:50px"> NAVIGATION SECTION</h1>
-
-    <center> </br> </br> </br>
-        </br>
-        <img src="lab1.jpg" style="width: 300px; height:200px; left: 10px; position:relative; right: 20px ">
-        <br />
-        <br />
-
-
-        <li> <a href="lab1.php">Hello World </a></li>
-        <br />
-        <br />
-
-
-
-        <img src="labtwo.jpg" style="width: 300px; height:200px; left: 10px; position:relative; right: 20px ">
-        <br />
-        <br />
-
-        <li><a href="lab2.php">Creating Basic PHP Script</a></li>
-
-        <br />
-        <br />
-
-        <img src="labthree.jpg" style="width: 300px; height:200px; left: 10px; position:relative; right: 20px ">
-        <br />
-        <br />
-
-        <li><a href="lab3.php">Working with Data Types and Operators</a></li>
-
-
-        </a> </br> </br>
-        <img src="lab4.jpg" style="width: 300px; height:200px; left: 10px; position:relative; right: 20px ">
-        <br />
-        <br />
-
-        <li><a href="lab4.php">Functions and Control Structures –Number to Words</a></li>
-
-        </a>
-
-        </br>
-        </br>
-
-        <img src="labfive.jpg" style="width: 300px; height:200px; left: 10px; position:relative; right: 20px ">
-        <br />
-        <br />
-        <li><a href="lab5.php">Iteration Structures</a></li>
-
-
-        </br> </br>
-        <img src="labsix.jpg" style="width: 300px; height:200px; left: 10px; position:relative; right: 20px ">
-        <br />
-        <br />
-        <li><a href="lab6.php">String Functions in PHP</a></li>
-
-        </br> </br>
-        <img src="labseven.png" style="width: 300px; height:200px; left: 10px; position:relative; right: 20px ">
-        <br />
-        <br />
-        <li><a href="lab7.php"> Regular Expression</a></li>
-
-        </br> </br>
-        <img src="labeight.jpg" style="width: 300px; height:200px; left: 10px; position:relative; right: 20px ">
-        <br />
-        <br />
-        <li><a href="lab8.php"> Array Manipulations (Word Counter)</a></li>
-
-
-
-        </a>
-
-
-</div>
-
-
-
-<div style="border: 4px solid black;margin: 40px; position:absolute;top:400px;  bottom: 400px;  width:900px; height: 1400px; left:550px;;background-image: url(https://i.pinimg.com/564x/8d/73/02/8d730284d6da8657bfc12192782d8115.jpg); background-size:cover;object-fit:contain">
-
-    <h1 style=" color:brown; font-family: 'PT Sans Narrow', sans-serif;  font-size: 50px; text-align:center; top:10px; text-indent:50px"> CONTENT SECTION </h1>
-    </br>
-    </br>
-    </br>
-
-    <center>
-
-        <?php
-
-        $text = (isset($_POST['text'])) ? $_POST['text'] : '';
-        echo '<form method="post">';
-        echo 'Enter text:<br />';
-        echo '<textarea name="text" style="width:500px">';
-        echo $text;
-        echo '</textarea><br />';
-        echo '<input type=submit value="Process">&nbsp;';
-        echo '<input type=reset>&nbsp;';
-        echo '</form>';
-        $text = preg_replace('/[^a-zA-Z0-9 ]/', '', trim($text));
-
-        $colors[] = '#00CCFF';
-        // No 1. Add at least 10 color values to array $colors
-        $colors[0] = '#000000';
-        $colors[1] = '#FF0000';
-        $colors[2] = '#FFA500';
-        $colors[3] = '#FFFF00';
-        $colors[4] = '#00FF00';
-        $colors[5] = '#0000FF';
-        $colors[6] = '#4B0082';
-        $colors[7] = '#8F00FF';
-        $colors[8] = '#964B00';
-        $colors[9] = '#FFFFFF';
-
-        if ($text <> '') {
-            // No. 2. Convert the $text to lowercase and assign to $text
-            $text = strtolower($text);
-
-            // No. 3. Explode $text into array andassign to $allwords
-            $allwords = explode(' ', $text);
-
-            // No. 4. Remove duplicate items and assign to $uniquewordlist
-            $uniquewordlist = array_unique($allwords);
-
-            // No. 5. sort the array $uniquewordlist
-            sort($uniquewordlist);
-
-            // No. 6. Check thefirst element of $uniquewordlist if equal to space
-            if ($uniquewordlist == '') {
-                // No. 7. Remove the first element from $uniquewordlist
-                $deleteditem = array_shift($uniquewordlist);
-            }
-
-            foreach ($uniquewordlist as $word)
-                $counter[$word] = 0;
-
-            $totalwords = 0;
-            foreach ($allwords as $word) {
-                if ($word != '') {
-                    $counter[$word]++;
-                    $totalwords++;
-                }
-            }
-
-            echo '<table border=1 style="width:500px">';
-            echo '<tr><td colspan=4 align=center>Array Manipulation -Word Counter</td></tr>';
-            echo '<tr><td>No.</td><td>Word</td><td>Frequency</td><td>Percent</td></tr>';
-            $i = 1;
-            foreach ($uniquewordlist as $word) {
-                // No. 8. Get the current element of array $colors (IAP function)
-                $color = current($colors);
-
-                $length = number_format($counter[$word] / $totalwords * 100.00, 2);
-                echo '<tr>';
-                echo '<td>', $i, '.</td>';
-                echo '<td>', $word, '</td>';
-                echo '<td>', $counter[$word], '</td>';
-                echo '<td style="background:', $color, '">', $length, '%</td>';
-                echo '</tr>';
-
-                // No. 9. Move the internal array pointer of $colors to next element
-                next($colors);
-
-                if (current($colors) == false) {
-                    // No. 10. Reset internal array pointer of $colors
-                    reset($colors);
-                }
-                $i++;
-            }
-            echo '</table>';
-        }
-        ?>
-
-
-    </center>
-</div>
-
-<footer>
-    <center>
-
-        <?php define("NAME", "Lindsay Daza"); ?>
-        <center>
-            <div style="border: 4px solid black; background-color:white;left:850px;top:1870px;  bottom:300px; height: 20px; width:500px; position:absolute">
-                <?php echo '&copy; ', date('Y'), ' ', NAME, ' :: Rundate ', date('m/d/Y'); ?>
-
-        </center>
-        </div>
-
-        <body style="background-image: url(https://i.pinimg.com/564x/77/50/8f/77508fbf73babb8a77e361553c9a16d5.jpg); background-size:contain;object-fit:contain">
-
-
-
-        </body>
+</html>
